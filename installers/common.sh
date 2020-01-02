7f2af6035c419f270d0431d0bbe026fade7131b8
Gergo Biacsko <biacskogergo@gmail.com>
2020-01-02 03:43:39 +0000

    Modified to use PHP7.3-cgi

--- installers/common.sh
+++ installers/common.sh
@@ -13,7 +13,7 @@ version=`sed 's/\..*//' /etc/debian_version`
 # php package to install 
 if [ "$version" -eq "10" ]; then
     version_msg="Raspbian 10.0 (Buster)"
-    php_package="php7.1-cgi"
+    php_package="php7.3-cgi"
 elif [ "$version" -eq "9" ]; then
     version_msg="Raspbian 9.0 (Stretch)" 
     php_package="php7.0-cgi" 
